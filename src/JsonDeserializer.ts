19df006a799a536d43f4fcecd8f5e3e98395a9a2
Tim van Dam <timvandamcs@gmail.com>
2020-05-28 17:35:13 +0200

    objects now also work

--- src/JsonDeserializer.ts
+++ src/JsonDeserializer.ts
@@ -98,9 +98,8 @@ function * ArrayStateMachine (): JsonValueParser {
 	let start = false
 	let chunk: string = ''
 	let stateMachine: JsonValueParser|null = null
-	let state = EArrayState.EXPECTING_VALUE as EArrayState
+	let state = EArrayState.EXPECTING_VALUE
 	while ((chunk = yield)) {
-		let i
 		if (!chunk || !chunk.length) continue
 		if (!start) {
 			if (chunk.charAt(0) === '[') {
@@ -108,9 +107,8 @@ function * ArrayStateMachine (): JsonValueParser {
 				chunk = chunk.slice(1)
 			} else throw new Error('Not an array!')
 		}
-		for (i = 0; i < chunk.length; i++) {
+		for (let i = 0; i < chunk.length; i++) {
 			const char = chunk.charAt(i)
-			console.log(char)
 
 			// If we're not reading at the moment the array might close & whitespaces are allowed
 			if (!stateMachine) {
@@ -118,30 +116,133 @@ function * ArrayStateMachine (): JsonValueParser {
 				if (char === ']') {
 					return {
 						result,
-						rest: chunk.slice(i + 1) // TODO: Make sure this is right
+						rest: chunk.slice(i + 1)
 					}
 				}
 			}
 
-			if (state === EArrayState.EXPECTING_COMMA) {
-				if (char !== ',') throw new Error('Invalid array!')
-				state = EArrayState.EXPECTING_VALUE
-				continue
-			}
-			// At this point we're expecting a value, so let's look for one!
-			stateMachine = stateMachine ?? chooseStateMachine(char)
-			if (!stateMachine) throw new Error(`Invalid character ${char} in array`)
+			switch (state) {
+				case EArrayState.EXPECTING_COMMA: {
+					if (char !== ',') throw new Error('Invalid array!')
+					state = EArrayState.EXPECTING_VALUE
+					continue
+				}
 
-			const { done, value } = stateMachine.next(char)
-			if (done) {
-				result.push((value as ReadResult).result)
-				stateMachine = null
-				chunk = chunk.slice(0, i + 1) + (value as ReadResult).rest + chunk.slice(i + 1) // if any characters were consumed, add them back
-				state = EArrayState.EXPECTING_COMMA
+				case EArrayState.EXPECTING_VALUE: {
+					// At this point we're expecting a value, so let's look for one!
+					stateMachine = stateMachine ?? chooseStateMachine(char)
+					if (!stateMachine) throw new Error(`Invalid character ${char} in array`)
+
+					// We have a state machine, lets provide the entire remaining chunk
+					const { done, value } = stateMachine.next(chunk.slice(i))
+
+					// If it's done we can simply start analyzing the rest
+					if (done) {
+						result.push((value as ReadResult).result)
+						stateMachine = null
+						chunk = (value as ReadResult).rest
+						i = -1 // -1 because i++ will set it to 0
+						state = EArrayState.EXPECTING_COMMA
+					}
+
+					break
+				}
+
+				default: throw new Error(`Invalid state ${state}`)
 			}
 		}
 	}
 	throw new Error('Invalid array!')
+}
+
+enum EObjectState {
+	EXPECTING_VALUE,
+	EXPECTING_KEY, // p much same as expecting string
+	EXPECTING_COLON,
+	EXPECTING_COMMA
+}
+
+function * ObjectStateMachine (): JsonValueParser {
+	const result: Record<string, JsonValue> = {}
+	let start = false
+	let chunk: string | null = null
+	let stateMachine: JsonValueParser | null = null
+	let state = EObjectState.EXPECTING_KEY as EObjectState
+	let key: string | null = null
+	while ((chunk = yield)) {
+		if (!chunk || !chunk.length) continue
+		if (!start) {
+			if (chunk.charAt(0) === '{') {
+				start = true
+				chunk = chunk.slice(1)
+			} else throw new Error('Not an object!')
+		}
+		for (let i = 0; i < chunk.length; i++) {
+			const char = chunk.charAt(i)
+
+			// Not reading? Check if the object ends
+			if (!stateMachine || (stateMachine && EObjectState.EXPECTING_KEY)) {
+				if (char.match(/\s/)) continue // ignore whitespaces
+				if (char === '}') {
+					return {
+						result,
+						rest: chunk.slice(i + 1)
+					}
+				}
+			}
+
+			switch (state) {
+				case EObjectState.EXPECTING_COMMA: {
+					if (char !== ',') throw new Error('Invalid object!')
+					state = EObjectState.EXPECTING_KEY
+					continue
+				}
+
+				case EObjectState.EXPECTING_COLON: {
+					if (char !== ':') throw new Error('Invalid object!')
+					state = EObjectState.EXPECTING_VALUE
+					continue
+				}
+
+				case EObjectState.EXPECTING_KEY: {
+					// Ensure that a string state machine is used
+					stateMachine = stateMachine ?? chooseStateMachine('"') as JsonValueParser
+
+					const { done, value } = stateMachine.next(chunk.slice(i))
+					if (done) {
+						key = (value as ReadResult).result as string
+						stateMachine = null
+						chunk = (value as ReadResult).rest
+						i = -1
+						state = EObjectState.EXPECTING_COLON
+					}
+
+					break
+				}
+
+				case EObjectState.EXPECTING_VALUE: {
+					stateMachine = stateMachine ?? chooseStateMachine(char)
+					if (!stateMachine) throw new Error(`Invalid character ${char} in object`)
+
+					const { done, value } = stateMachine.next(chunk.slice(i))
+					if (done) {
+						if (!key) throw new Error('Invalid object')
+						result[key] = (value as ReadResult).result
+						stateMachine = null
+						chunk = (value as ReadResult).rest
+						key = null
+						i = -1
+						state = EObjectState.EXPECTING_COMMA
+					}
+
+					break
+				}
+
+				default: throw new Error(`Invalid state ${state}`)
+			}
+		}
+	}
+	throw new Error('Invalid object!')
 }
 
 /**
@@ -172,6 +273,11 @@ function chooseStateMachine (char: string): JsonValueParser|null {
 			stateMachine = ArrayStateMachine()
 			break
 		}
+
+		case '{': {
+			stateMachine = ObjectStateMachine()
+			break
+		}
 	}
 
 	if (stateMachine) stateMachine.next() // initialize the state machine.
@@ -181,7 +287,7 @@ function chooseStateMachine (char: string): JsonValueParser|null {
 /**
  * Parses a JSON string (in the form of a buffer) as a JS object.
  */
-class JsonDeserializer extends Duplex {
+export default class JsonDeserializer extends Duplex {
 	// we only need to store one state machine. if the provided thing is an object the object state machine will simply
 	// use other state machines to read the other stuff
 	private stateMachine: JsonValueParser|null = null
@@ -246,16 +352,12 @@ class JsonDeserializer extends Duplex {
 		}
 
 		// Feed characters into the state machine
-		for (const char of str) {
-			const { done, value } = this.stateMachine.next(char)
-
-			// If the value has successfully been parsed push it or save it for later if it cant be pushed yet
-			if (done) {
-				this.result = (value as ReadResult).result
-				this.canPush = this.canPush && this.push(this.result) && this.push(null) && (this.resultRead = true)
-				this.end()
-				break
-			}
+		const { done, value } = this.stateMachine.next(str)
+		// If the value has successfully been parsed push it or save it for later if it cant be pushed yet
+		if (done) {
+			this.result = (value as ReadResult).result
+			this.canPush = this.canPush && this.push(this.result) && this.push(null) && (this.resultRead = true)
+			this.end()
 		}
 		callback()
 	}
@@ -268,4 +370,4 @@ j.pipe(new Writable({
 		console.log(chunk)
 	}
 }))
-j.end('["hello", 1, 3.14e4]')
+j.end('{"hello":[123, "321"]}')
